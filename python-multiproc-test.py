fe7f087bff3900b7adc33eb8f0e03cf83e690db1
Brian Timar <timarbrian@gmail.com>
2019-06-18 14:21:46 -0400

    update python

--- python-multiproc-test.py
+++ python-multiproc-test.py
@@ -1,12 +1,16 @@
-from multiprocessing import Pool
+from multiprocessing import Pool, Process
 import time
 
-ncore = 20
+ncore = 10
 
 def use_core(i):
     print("Starting %d"%i)
     time.sleep(13)
     print("Finished %d"%i)
 
-p = Pool(ncore)
-p.map(use_core, range(ncore))
+jobs = []
+for j in range(ncore):
+    p=Process(target=use_core, args=(j,))
+    jobs.append(p)
+    p.start()
+    
